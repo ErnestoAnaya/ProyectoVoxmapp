2985754258ce0eebe761956e17efa9fe034190e1
ErnestoAnaya <77375206+ErnestoAnaya@users.noreply.github.com>
2021-05-10 00:21:03 -0500

    Add files via upload

---
+++ creacion_tablas.sql
@@ -0,0 +1,121 @@
+CREATE TABLE cuestionario (
+	cuestionario_id numeric constraint pk_cuestionario primary key
+);
+create sequence cuestionario_id_cuestionario_seq start 1 increment 1 ;
+ALTER TABLE cuestionario ALTER COLUMN cuestionario_id SET DEFAULT nextval('cuestionario_id_cuestionario_seq');
+
+
+
+CREATE TABLE seguimiento (
+	seguimiento_id numeric constraint pk_seguimiento primary key,
+	regular_tracking boolean NOT null,
+	MOPH_report_frecuency VARCHAR(50) NOT null,
+	cuestionario_id numeric REFERENCES cuestionario (cuestionario_id) 
+);
+create sequence seguimiento_id_seguimiento_seq start 1 increment 1 ;
+ALTER TABLE seguimiento ALTER COLUMN seguimiento_id SET DEFAULT nextval('seguimiento_id_seguimiento_seq');
+
+
+CREATE TABLE infraestructura (
+	infraestructura_id numeric constraint pk_infraestructura primary key,
+	screening_implemeted boolean NOT null,
+  	awareness_campaigns boolean NOT null,
+  	test_COVID_capabilities boolean NOT null,
+  	test_result_speed int NOT null,
+  	resources_received_last_month varchar(50),
+  	cuestionario_id numeric REFERENCES cuestionario (cuestionario_id) 
+);
+create sequence infraestructura_id_infraestructura_seq start 1 increment 1 ;
+ALTER TABLE infraestructura ALTER COLUMN infraestructura_id SET DEFAULT nextval('infraestructura_id_infraestructura_seq');
+
+
+CREATE TABLE control_ (
+	control_id numeric constraint pk_control primary key,
+	update_status VARCHAR(50) NOT null,
+	problem VARCHAR(50) NOT null,
+	accion VARCHAR(50) NOT null,
+ 	cuestionario_id numeric REFERENCES cuestionario (cuestionario_id) 
+
+);
+create sequence control_id_control_seq start 1 increment 1 ;
+ALTER TABLE control_ ALTER COLUMN control_id SET DEFAULT nextval('control_id_control_seq');
+
+
+CREATE TABLE casos_covid (
+	casos_covid_id numeric constraint pk_casos_covid primary key,
+	PHC_reffered_cases numeric NOT null,
+	positive_tests_last_month numeric NOT null,
+	intensive_care numeric NOT null,
+	deaths_last_month numeric NOT null,
+	non_COVID_deaths numeric NOT null,
+	recovered_patients numeric NOT null,
+ 	cuestionario_id numeric REFERENCES cuestionario (cuestionario_id) 
+
+);
+create sequence casos_covid_id_casos_covid_seq start 1 increment 1 ;
+ALTER TABLE casos_covid ALTER COLUMN casos_covid_id SET DEFAULT nextval('casos_covid_id_casos_covid_seq');
+
+
+CREATE TABLE reservas (
+	reservas_id numeric constraint pk_reservas primary key,
+	oxygen_reserves VARCHAR(50) NOT null,
+	antipyretics_reserves VARCHAR(50) NOT null,
+	anesthetics_and_muscular_relaxants VARCHAR(50) NOT null,
+	alcohol_reserves_and_handsoap VARCHAR(50) NOT null,
+	personal_disposable_masks VARCHAR(50) NOT null,
+	personal_vinyl_gloves VARCHAR(50) NOT null,
+	personal_disposable_hats VARCHAR(50) NOT null,
+	personal_disposable_aprons VARCHAR(50) NOT null,
+	personal_visors VARCHAR(50) NOT null,
+	personal_disposable_shoe_covers VARCHAR(50) NOT null,
+	test_kits VARCHAR(50) NOT null,
+	num_test_kits numeric NOT null,
+	respiratory_ventilator_machines numeric NOT null,
+	cuestionario_id numeric REFERENCES cuestionario (cuestionario_id) 
+);
+create sequence reservas_id_reservas_seq start 1 increment 1 ;
+ALTER TABLE reservas ALTER COLUMN reservas_id SET DEFAULT nextval('reservas_id_reservas_seq');
+
+create table hospital(
+	hospital_id numeric constraint pk_hospital primary key,
+	MOPH_number numeric not null,
+  	hospital_name varchar (50) not null,
+  	district varchar (50) not null,
+  	province varchar (50) not null,
+  	hospital_type varchar (50) not null,
+  	longitude float8 not null,
+  	latitude float8 not null,
+  	altitude float8 not null,
+  	creation_date timestamp not null,
+  	--photo photo not null,
+  	personal_VM_id numeric not null,
+  	num_doctors numeric not null,
+  	num_staff numeric not null,
+  	type_ char
+);
+create sequence hospital_id_hospital_seq start 1 increment 1 ;
+ALTER TABLE hospital ALTER COLUMN hospital_id SET DEFAULT nextval('hospital_id_hospital_seq');
+
+
+create table personal(
+	personal_id numeric constraint pk_personal primary key,
+	first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    number_ numeric not null,
+    
+    hospital_id numeric REFERENCES hospital (hospital_id) 
+);
+create sequence personal_id_personal_seq start 1 increment 1 ;
+ALTER TABLE personal ALTER COLUMN personal_id SET DEFAULT nextval('personal_id_personal_seq');
+
+
+create table update_(
+	update_id int constraint pk_update primary key,
+    update_date timestamp not null,
+    
+    personal_id numeric REFERENCES personal (personal_id),
+    hospital_id numeric REFERENCES hospital (hospital_id),
+    cuestionario_id numeric REFERENCES cuestionario (cuestionario_id) 
+);
+create sequence update_id_update_seq start 1 increment 1 ;
+ALTER TABLE update_ ALTER COLUMN update_id SET DEFAULT nextval('update_id_update_seq');
