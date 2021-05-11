7636fb88d01807b9834d92832b14801b8c72b397
ErnestoAnaya <77375206+ErnestoAnaya@users.noreply.github.com>
2021-05-10 22:21:23 -0500

    Update creacion_tablas.sql

--- creacion_tablas.sql
+++ creacion_tablas.sql
@@ -115,3 +115,44 @@ create table personal_voxmapp(
   	number_ numeric
 );
 
+--BORRADO DE DATOS
+
+
+
+
+
+
+-- BORRADO DE OBJETOS
+
+-- DROP TABLES
+
+drop table personal_voxmapp;
+drop table seguimiento;
+drop table reservas;
+drop table infraestructura;
+drop table control_;
+drop table casos_covid;
+drop table update_;
+drop table personal;
+drop table hospital;
+
+-- DROP SEQUENCES
+
+drop sequence personal_id_personal_seq;
+drop sequence update_id_update_seq;
+drop sequence seguimiento_id_seguimiento_seq;
+drop sequence infraestructura_id_infraestructura_seq;
+drop sequence control_id_control_seq;
+drop sequence casos_covid_id_casos_covid_seq;
+drop sequence reservas_id_reservas_seq;
+
+--BORRADO DE DATOS
+
+delete from hospital;
+delete from personal;
+delete from control_;
+delete from update_;
+delete from seguimiento;
+delete from infraestructura;
+delete from casos_covid;
+delete from reservas;
