d7cf96e0b59c2fd70875af1bfeaff75022dfe4a5
jorcazas <69361158+jorcazas@users.noreply.github.com>
2021-05-11 14:48:37 -0500

    Create script_drop_objects.sql

---
+++ script_drop_objects.sql
@@ -0,0 +1,24 @@
+
+-- DROP TABLES
+
+drop table personal_voxmapp;
+drop table seguimiento;
+drop table reservas;
+drop table infraestructura;
+drop table control_;
+drop table casos_covid;
+drop table update_;
+drop table personal;
+drop table hospital;
+
+
+-- DROP SEQUENCES
+
+drop sequence personal_id_personal_seq;
+drop sequence update_id_update_seq;
+drop sequence seguimiento_id_seguimiento_seq;
+drop sequence infraestructura_id_infraestructura_seq;
+drop sequence control_id_control_seq;
+drop sequence casos_covid_id_casos_covid_seq;
+drop sequence reservas_id_reservas_seq;
+
