5137f3dd688ee94b8e335ae1cd86bddf702cd830
jorcazas <69361158+jorcazas@users.noreply.github.com>
2021-05-11 14:54:31 -0500

    Create script_drop_data.sql

---
+++ script_drop_data.sql
@@ -0,0 +1,11 @@
+
+--DELETE DATA
+delete from personal_voxmapp;
+delete from seguimiento;
+delete from reservas;
+delete from infraestructura;
+delete from control_;
+delete from casos_covid;
+delete from update_;
+delete from personal;
+delete from hospital;
