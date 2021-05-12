8b737286d2a84b16033b6a62ecfb2c9ffb82d03d
jorcazas <69361158+jorcazas@users.noreply.github.com>
2021-05-11 19:12:43 -0500

    Add files via upload

---
+++ ProyectoFinalBaseDatos/ProyectoFinalBaseDatos/Admin.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoFinalBaseDatos
+{
+    /// <summary>
+    /// Lógica de interacción para Admin.xaml
+    /// </summary>
+    public partial class Admin : Window
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+        private void Resultados_Click(object sender, RoutedEventArgs e)
+        {
+            
+            System.Diagnostics.Process.Start("https://docs.google.com/spreadsheets/d/1NrnirdVqhgThFQqfcyC8YquR7ciKqK_j6qingCOUF14/edit?resourcekey#gid=1215732058");
+
+        }
+
+        private void Actualizar_Click(object sender, RoutedEventArgs e)
+        {
+            ConectarBD con = new ConectarBD();
+            string status_act = con.Run_cmd();
+
+            if (status_act == "")
+                MessageBox.Show("Por favor, revisa tu conexión a internet");
+            else
+                MessageBox.Show(status_act);
+        }
+
+        private void Atrás_Click(object sender, RoutedEventArgs e)
+        {
+            Window main = new MainWindow();
+            this.Hide();
+            main.Show();
+        }
+    }
+}
